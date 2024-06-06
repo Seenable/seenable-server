e60ccd23c2e091dbadcf71f2a5890938e68f8426
Alice-2525 <2220048@ecc.ac.jp>
2024-06-06 10:42:06 +0900

    feature DBにアクセスする処理を書きました。

---
+++ db.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+class DB(object):
+
+    def __init__(self, dbName, collectionName):
+        self.client = MongoClient('seenable-db', 27017, username='root', password='password')
+        self.db = self.client[dbName] #DB名を設定
+        self.collection = self.db.get_collection(collectionName)
+
+    def collection_names(self):
+        return self.db.collection_names()
+
+    def list_collection_names(self):
+        return self.db.list_collection_names()
+
+    def find(self, projection=None,filter=None, sort=None):
+        return self.collection.find(projection=projection,filter=filter,sort=sort)
+
+# mongo = DB('seenable' , 'students')
+# find = mongo.find(filter={'_id': 1})
+# print('-------------------find-------------------')
+# print(type(find))
+# for doc in find:
+#     print(doc)

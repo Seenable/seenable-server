8ed436439d5759fe0fa7a9f151160e20f55a367e
Alice-2525 <2220048@ecc.ac.jp>
2024-06-10 16:41:14 +0900

    add 　" / " のAPIをたたいた時にデモ画像の顔認証を行い名前を取得する処理を追加した

---
+++ app/feature/face_recognition_summary.py
@@ -0,0 +1,37 @@
+import face_recognition
+from PIL import Image, ImageDraw
+import numpy as np
+import glob
+import os
+import app.feature.db as db
+
+class FaceRecognitionSummary() :
+    known_face_encodings = []
+    known_face_names = []
+
+    def __init__(self):
+        files = glob.glob("./app/images/train/*")
+        for file in files:
+            # print(file)
+            # 画像を読み込み、顔の特徴値を取得する
+            train_image = face_recognition.load_image_file(file)
+            train_face_location = face_recognition.face_locations(train_image, model="hog")
+            self.known_face_encodings.append(face_recognition.face_encodings(train_image, train_face_location)[0])
+            self.known_face_names.append(os.path.split(file)[1].split('.')[0])
+
+    def __receive_test_image(self, imgName):
+        test_img = face_recognition.load_image_file("./app/images/test/" + imgName)
+        test_img_location = face_recognition.face_locations(test_img, model="hog")
+        test_img_encoding = face_recognition.face_encodings(test_img, test_img_location)[0]
+        return test_img_encoding
+
+    def match_face(cls, imgName):
+        test_img_encoding = cls.__receive_test_image(imgName)
+        matches = face_recognition.compare_faces(cls.known_face_encodings, test_img_encoding)
+        id = 0
+        face_distances = face_recognition.face_distance(cls.known_face_encodings, test_img_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            id = cls.known_face_names[best_match_index]
+        # print("マッチした画像のID：" + id)
+        return id
--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
+from app.feature.face_recognition_summary import FaceRecognitionSummary
 from fastapi import FastAPI
+import app.feature.db as db
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    # return {"Hello": "World"}
+    face_recognition_summary = FaceRecognitionSummary()
+    id = face_recognition_summary.match_face("IMG_2323.jpg")
+    mongo = db.DB('seenable' , 'students')
+    find = mongo.find(filter={'_id': int(id)})
+    name = "no"
+    for doc in find:
+        print(doc['name'])
+        name = doc['name']
+    return { "name" : name}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+

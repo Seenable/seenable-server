263d540524ef59c5bf933231cb9b95de97e5bc72
Alice-2525 <2220048@ecc.ac.jp>
2024-06-13 15:38:21 +0900

    add 送られてくる画像形式がBase64のため、顔認証に使える形式に変換する機能を追加。

--- app/feature/face_recognition_summary.py
+++ app/feature/face_recognition_summary.py
@@ -3,7 +3,6 @@ from PIL import Image, ImageDraw
 import numpy as np
 import glob
 import os
-import app.feature.db as db
 
 class FaceRecognitionSummary() :
     known_face_encodings = []
@@ -12,15 +11,13 @@ class FaceRecognitionSummary() :
     def __init__(self):
         files = glob.glob("./app/images/train/*")
         for file in files:
-            # print(file)
             # 画像を読み込み、顔の特徴値を取得する
             train_image = face_recognition.load_image_file(file)
             train_face_location = face_recognition.face_locations(train_image, model="hog")
             self.known_face_encodings.append(face_recognition.face_encodings(train_image, train_face_location)[0])
             self.known_face_names.append(os.path.split(file)[1].split('.')[0])
 
-    def __receive_test_image(self, imgName):
-        test_img = face_recognition.load_image_file("./app/images/test/" + imgName)
+    def __receive_test_image(self, test_img):
         test_img_location = face_recognition.face_locations(test_img, model="hog")
         test_img_encoding = face_recognition.face_encodings(test_img, test_img_location)[0]
         return test_img_encoding
@@ -33,5 +30,5 @@ class FaceRecognitionSummary() :
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             id = cls.known_face_names[best_match_index]
-        # print("マッチした画像のID：" + id)
+
         return id
